0c0a71ad42cc34a616e77d543fc1e13951c6ea7c
girraj kushwah <girrajkushwah7247@gmail.com>
2024-12-18 12:45:17 +0530

    Add files via upload
    Student Record Management Form

---
+++ form.js
@@ -0,0 +1,9 @@
+let contant = document.querySelector(".data");
+let BTN1 = document.querySelector("#btn1");
+let BTN2 = document.querySelector("#btn2");
+let BTN3 = document.querySelector("#btn3");
+let BTN4 = document.querySelector("#btn4");
+let BTN5 = document.querySelector("#btn5");
+
+appendChild(element);
+
